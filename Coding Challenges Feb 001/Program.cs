d2920d7fff96891af971a38af4e70421cf89cfd9
MidWest005 <mlwest5@gmail.com>
2024-02-22 04:48:04 -0600

    2/22/2024 adding new challenge

--- Coding Challenges Feb 001/Program.cs
+++ Coding Challenges Feb 001/Program.cs
@@ -90,7 +90,45 @@
             return foo;
         }
 
-        //Next Problem Here
+        /*Edabit Hard Consecutive Numbers
+         * Create a function that determines whether elements in an array 
+         * can be re-arranged to form a consecutive list of numbers where 
+         * each number appears exactly once.
+
+            Examples:
+
+            Cons([5, 1, 4, 3, 2]) ➞ true
+            // Can be re-arranged to form [1, 2, 3, 4, 5]
+
+            Cons([5, 1, 4, 3, 2, 8]) ➞ false
+
+            Cons([5, 6, 7, 8, 9, 9]) ➞ false
+            // 9 appears twice
+         * 
+         * 
+         */
+
+        public static bool Cons(int[] arr)
+        {
+            bool ans = true;
+            Array.Sort(arr);
+            Dictionary<int, bool> foo = new Dictionary<int, bool>();
+            foo.Add(arr[0], false);
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] == (arr[i - 1] + 1) && foo.ContainsKey(arr[i]) == false)
+                {
+                    foo.Add(arr[i], false);
+                }
+                else
+                {
+                    ans = foo[arr[i - 1]];
+                    break;
+                }
+            }
+
+            return ans;
+        }
 
     }
 }
