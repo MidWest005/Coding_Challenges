65f143a3a71fabc642868a580366069e2569b451
MidWest005 <mlwest5@gmail.com>
2024-02-22 04:08:42 -0600

    Initial commit for Feb 2024 Coding Challenges

---
+++ Coding Challenges Feb 001/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Coding_Challenges_Feb_001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> player = new();
+
+            player.Add("Jordan", 23);
+            player.Add("Pippen", 33);
+
+            Console.WriteLine($"{player["Jordan"]}"); //
+        }
+
+
+
+        /*Edabit Hard - Next Prime
+             * Next Prime
+            Given an integer, create a function that returns the next prime. If the number is prime, return the number itself.
+
+            Examples:
+            NextPrime(12) ➞ 13
+
+            NextPrime(24) ➞ 29
+
+            NextPrime(11) ➞ 11
+            11 is a prime, so we return the number itself.
+        */
+        int NextPrime(int num)
+        {
+            bool isPrime = false;
+            int tic = 0;
+            do
+            {
+                tic = 0;
+                for (int i = 2; i < num / 2 + 1; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        tic++;
+                        break;
+                    }
+                }
+                num++;
+            } while (tic > 0);
+
+            return num - 1;
+        }
+
+
+
+
+        /*Edabit Hard - Sock Pairs
+         * Joseph is in the middle of packing for a vacation. He's having a bit of trouble finding all of his socks, though.
+
+            Write a function that returns the number of sock pairs he has. 
+            A sock pair consists of two of the same letter, such as "AA". 
+            The socks are represented as an unordered sequence.
+
+            Examples
+            SockPairs("AA") ➞ 1
+
+            SockPairs("ABABC") ➞ 2
+
+            SockPairs("CABBACCC") ➞ 4
+
+            Notes:
+            **If given an empty string (no socks in the drawer), return 0.
+            **There can be multiple pairs of the same type of sock, such as two pairs of CC for the last example.
+         */
+        public static int SockPairs(string socks)
+        {
+            Dictionary<char, int> sockCount = new Dictionary<char, int>();
+            foreach (var i in socks)
+            {
+                if (sockCount.ContainsKey(i))
+                {
+                    sockCount[i]++;
+                }
+                else
+                {
+                    sockCount.Add(i, 1);
+                }
+            }
+            int foo = 0;
+            foreach (var k in sockCount.Values)
+            {
+                foo += k / 2;
+            }
+            return foo;
+        }
+
+        //Next Problem Here
+
+    }
+}
